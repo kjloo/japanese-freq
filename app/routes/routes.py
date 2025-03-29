116d7fd1161f3709a654438ef2567fb66d997339
kjloo <loo.kaleb@gmail.com>
2025-03-29 13:08:53 -1000

    running

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,50 +1,8 @@
-import os
-from flask import Blueprint, render_template, send_from_directory, jsonify, request
+from flask import Blueprint, render_template
 from service import japanese_freq_service
 
 # Blueprint for routes
 app_routes = Blueprint('routes', __name__)
-image_dir = os.environ.get('IMAGE_DIR')
-
-
-@app_routes.route('/api/board', methods=['GET'])
-def get_board():
-    resp = game_state_service.get_game_state()
-    return resp.to_player_json()
-
-
-@app_routes.route("/api/spymaster", methods=["GET"])
-def get_spymaster_key():
-    """
-    API endpoint to return a randomly generated 5x5 spymaster key.
-    """
-    resp = game_state_service.get_game_state()
-    return resp.to_master_json()
-
-
-@app_routes.route("/api/guess", methods=["POST"])
-def guess():
-    """
-    API endpoint to submit a guess
-    """
-    data = request.get_json()
-    guess: int = data.get("guess")
-    resp = game_state_service.submit_guess(guess)
-    return resp.to_player_json()
-
-
-@app_routes.route("/api/reset", methods=["POST"])
-def reset_board():
-    """
-    API endpoint to start a new game.
-    """
-    game_state_service.reset_game_state()
-    return jsonify({"message": "Game state reset successfully"}), 200
-
-
-@app_routes.route('/images/<path:filename>')
-def get_image(filename):
-    return send_from_directory(image_dir, filename)
 
 
 @app_routes.route("/")
