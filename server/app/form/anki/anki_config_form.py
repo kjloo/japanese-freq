1dc0e704e16272a274206654af623bec93db2ee6
kjloo <loo.kaleb@gmail.com>
2025-04-27 14:13:04 -1000

    add files

---
+++ server/app/form/anki/anki_config_form.py
@@ -0,0 +1,34 @@
+from typing import override
+
+from app.form.base_form import BaseForm
+from app.model.anki.anki_config import AnkiConfig
+
+
+class AnkiConfigForm(BaseForm):
+
+    deck_id: int = -1
+    kanji: str = ""
+    definition: str = ""
+
+    def __init__(self, json_data: dict[str]):
+        self.deck_id = json_data.get("deck_id", -1)
+        self.kanji = json_data.get("kanji", "")
+        self.definition = json_data.get("definition", "")
+        super().__init__()
+
+    def to_model(self) -> AnkiConfig:
+        return AnkiConfig(self.deck_id, self.kanji, self.definition)
+
+    @override
+    def _validate(self):
+        if self.deck_id == -1:
+            raise ValueError("Deck ID is required.")
+        if not self.kanji:
+            raise ValueError("Kanji field is required.")
+        if not self.definition:
+            raise ValueError("Definition field is required.")
+
+    def __repr__(self):
+        return (f"AnkiConfig(deck={self.deck_id}, "
+                f"kanji={self.kanji}, "
+                f"definition={self.definition})")
